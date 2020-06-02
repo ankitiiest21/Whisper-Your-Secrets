c6566eef1563d4d73eba215b9698616598a868b0
ankitkrsingh56 <ankitsinghix05@gmail.com>
2020-06-02 15:57:40 +0530

    Updated callbackURL

--- app.js
+++ app.js
@@ -174,7 +174,7 @@ app.post("/submit",function(req,res){
 });
 
 let port = process.env.PORT;
-if(port==NULL||port=="") port=3000;
+if(port==null||port=="") port=3000;
 
 app.listen(port,function(){
  console.log("Server has started.");
